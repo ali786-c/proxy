fb0811b93319fc339e66ea2c5ad7b44f17743a8f
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-02-22 21:21:05 +0500

    Phase 6.3 Final and updated cPanel path

--- api/app/Models/TicketMessage.php
+++ api/app/Models/TicketMessage.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class TicketMessage extends Model
 {
-    protected $fillable = ['support_ticket_id', 'user_id', 'message'];
+    protected $fillable = ['support_ticket_id', 'user_id', 'message', 'is_admin_reply'];
     
     public function ticket() { return $this->belongsTo(SupportTicket::class); }
     public function user() { return $this->belongsTo(User::class); }
