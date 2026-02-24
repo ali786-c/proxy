190e45425447c795bd08402d539f012ee9983a5d
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-02-24 22:39:04 +0500

    fix: AdminController missing UptimeRecord class import

--- api/app/Http/Controllers/AdminController.php
+++ api/app/Http/Controllers/AdminController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use App\Models\Order;
 use App\Models\WalletTransaction;
 use App\Models\UsageLog;
+use App\Models\UptimeRecord;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
