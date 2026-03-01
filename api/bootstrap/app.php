d25aa550e2b28b6cfd67c3ab35cc267791714a45
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-03-01 19:22:50 +0500

    fix: remove invalid exception method causing crash

--- api/bootstrap/app.php
+++ api/bootstrap/app.php
@@ -26,10 +26,5 @@ return Application::configure(basePath: dirname(__DIR__))
         $schedule->command('referral:process-pending')->daily();
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->shouldRenderHtmlWhen(function ($request, $e) {
-            if ($request->is('api/*')) {
-                return false;
-            }
-            return $request->expectsJson();
-        });
+        //
     })->create();
