0034949b827e1a49db9725ed0e7edecb91eec451
Muhammad Aliyan <aliyantarar4@gmail.com>
2026-03-03 21:46:29 +0500

    chore: add diagnostic test route to debug 404 on live

--- api/routes/web.php
+++ api/routes/web.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/test-route', function () {
+    return response()->json(['message' => 'Web route is working']);
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
